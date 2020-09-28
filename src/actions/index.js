205f7e7e66a5e08deccc005cc17165ea89ed2894
Ian Scott <chesnekov@gmail.com>
2020-09-28 14:27:46 -0700

    add makeApiCall function to actions/index.js and removed it from our headline class component
    Co-authored-by: NoelKirkland <noelkirkland@gmail.com>

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,3 +13,40 @@ export const getHeadlinesFailure = (error) => ({
   type: c.GET_HEADLINES_FAILURE,
   error
 });
+
+export const makeApiCall = () =>  {
+  return dispatch => {
+    dispatch(requestHeadlines());
+    return fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${process.env.REACT_APP_API_KEY}`)
+    .then(response => response.json)
+    .then(
+      (jsonifiedResponse) => {
+        dispatch(getHeadlinesSuccess(jsonifiedResponse.results));
+      })
+    .catch((error) => {
+      dispatch(getHeadlinesFailure(error));
+      }); 
+  }
+}
+
+/* 
+
+  makeApiCall = () => {
+    fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${process.env.REACT_APP_API_KEY}`)
+    .then(response => response.json())
+    .then(
+      (jsonifiedResponse) => {
+        this.setState({
+          isLoaded: true,
+          headlines: jsonifiedResponse.results
+        });
+      })
+      .catch((error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      });
+  }
+  
+*/
