562b3d08884c05a2fef459e3e4c9b122a5e6ddae
Ian Scott <chesnekov@gmail.com>
2020-09-28 11:28:33 -0700

    adds components sub folder, moves app.js and headlines into the folder. Also Noel is here and he is da bomb-om!
    Co-authored-by: NoelKirkland <noelkirkland@gmail.com>

---
+++ src/components/Headlines.js
@@ -0,0 +1,20 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+class Headlines extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state= {
+      error: null,
+      isLoaded: false,
+      headlines: []
+    };
+  }
+  
+  
+  render() {
+  
+  }
+}
+
+export default Headlines;
